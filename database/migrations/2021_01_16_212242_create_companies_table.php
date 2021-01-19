0adf0264e5b97a724469ef77248fe3af26135b2c
William Tomé <william.tomenh@gmail.com>
2021-01-19 20:04:45 -0300

    commit inicial

---
+++ database/migrations/2021_01_16_212242_create_companies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 45);
+            $table->string('manager', 45);
+            $table->string('cnpj', 45);
+            $table->string('image', 150);
+            $table->tinyInteger('status');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
