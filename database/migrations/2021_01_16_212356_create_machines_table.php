7bc16101ed03fe7cb0d2868a95ad52a92c5067d3
William Tomé <william.tomenh@gmail.com>
2021-01-20 21:49:30 -0300

    Adicionado colunas para a tabela machines.

--- database/migrations/2021_01_16_212356_create_machines_table.php
+++ database/migrations/2021_01_16_212356_create_machines_table.php
@@ -15,6 +15,11 @@ class CreateMachinesTable extends Migration
     {
         Schema::create('machines', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 45);
+            $table->decimal('value_throttle', 5, 2);
+            $table->decimal('value_working', 5, 2);
+            $table->string('image', 150)->nullable();
+            $table->tinyInteger('status');
             $table->timestamps();
             $table->softDeletes();
         });
