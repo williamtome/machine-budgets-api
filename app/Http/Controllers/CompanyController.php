8c9a4007c5f9240165c6674ca42993c780307122
William Tomé <william.tomenh@gmail.com>
2021-01-22 23:17:18 -0300

    Criados os métodos index e store.

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -2,9 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
 use Illuminate\Http\Request;
 
 class CompanyController extends Controller
 {
-    //
+    /**
+     * @var Company
+     */
+    private $company;
+
+    public function __construct(Company $company)
+    {
+        $this->company = $company;
+    }
+
+    /**
+     * Show all the companies.
+     *
+     * Retrieve and show all the companies.
+     *
+     * @return json
+     **/
+    public function index()
+    {
+        $companies = $this->company->all();
+
+        return response()->json($companies);
+    }
+
+    /**
+     * Store company.
+     *
+     * Store data of the company.
+     *
+     * @param Request $request Request data of the company
+     * @return type
+     * @throws conditon
+     **/
+    public function store(Request $request)
+    {
+        dd($request->all());
+    }
+
 }
