1d48e0385c0c568f137254d3da2b7ca8c6948fa2
William Tomé <william.tomenh@gmail.com>
2021-01-23 10:41:20 -0300

    Adicionado os campos da tabela companies disponíveis para mass assignment.

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title', 'manager', 'cnpj', 'image', 'status'
+    ];
 }
